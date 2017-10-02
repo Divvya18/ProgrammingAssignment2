07e68ac69390aaa3c9ada997ffce3757ee6c4300
Divvya18 <31815619+Divvya18@users.noreply.github.com>
2017-10-02 23:38:22 +0530

    Update and rename Cachematrix.R to ProgCachematrix.R

--- ProgCachematrix.R
+++ ProgCachematrix.R
@@ -1,12 +1,16 @@
-makeCacheMatrix <- function(mat = matrix()){
+#using the makeCacheMatrix function to set and get the matrix and inverse in it
+makeCacheMatrix <- function(mat = matrix()){#
+ ## initializing the inverse property
 in <- NULL
 set <- function(matrix){
 mat <<- matrix
 in <<- NULL
 }
+ ## using methods to get the matrix
 get <- function() {
 mat
 }
+  ##setting inverse matrix
 setInv <- function(inverse){
 in <<- inverse
 }
@@ -16,7 +20,8 @@ in
 
 list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
-
+##cacheSolve Function inverses the matrix 
+cacheSolve <- function(x,...){
 mat <- x$getInv()
 if(!is.null(mat) ) {
 message('receiving cached data')
